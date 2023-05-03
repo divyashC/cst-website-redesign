fd8b36fe9ad9a4986e59a3782a0d841c25d6bcdb
Pravaat Chhetri <81762189+PravaatChhetri@users.noreply.github.com>
2023-05-03 21:37:18 +0600

    footer is responsive

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
 	return (
 		// <div className="fixed bottom-0 w-full py-8 bg-gray-200">
 		<div className="py-8 bg-gray-200">
-			<div class="grid grid-cols-3 max-w-5xl mx-auto gap-2">
-				<div class="p-4">
+			<div class="grid grid-row md:grid-cols-3 max-w-5xl mx-auto gap-2">
+				<div class="p-4 flex flex-col items-center">
 					<h3 class="font-bold text-gray-800 mb-4 text-xl">Quick Links</h3>
-					<div className="flex flex-row">
+					<div className="flex flex-row ">
 						<div class="text-gray-500 space-y-2 flex flex-col mr-12">
 							<a
 								href="https://ims.rub.edu.bt"
@@ -78,9 +78,9 @@ const Footer = () => {
 				<div class="flex items-center justify-center">
 					<img src={CSTLogo} alt="CST Logo" height={120} width={120} />
 				</div>
-				<div class="p-4">
+				<div class="p-4 flex flex-col items-center justify-center">
 					<h3 class="font-bold text-gray-800 mb-4 text-xl">Address</h3>
-					<div class="text-gray-500 space-y-1">
+					<div class="text-gray-500 space-y-1 text-center md:text-left">
 						<p>College of Science and Technology</p>
 						<p>Rinchending, Phuentsholing</p>
 						<p>Chhukha, Bhutan</p>
