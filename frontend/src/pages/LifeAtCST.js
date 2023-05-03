74de96f3891126a060692be654723cbdddcc25b2
popss <pemalw99@gmail.com>
2023-05-03 21:04:58 +0530

    Update LifeAtCST.js

--- frontend/src/pages/LifeAtCST.js
+++ frontend/src/pages/LifeAtCST.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LifeAtCST = () => {
-	return 
+	return (
 	<div>LifeAtCST
 		<div className="drawer drawer-mobile">
   			<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -12,14 +12,16 @@ const LifeAtCST = () => {
   			</div> 
   			<div className="drawer-side">
   			  <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-  			  <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+  			  <ul className="menu p-4 w-80 bg-base-100 text-base-c
+			  ontent">
   			    <li><a>Sidebar Item 1</a></li>
   			    <li><a>Sidebar Item 2</a></li>
   			  </ul>
 
   			</div>
 		</div>
-	</div>;
+	</div>
+	);
 };
 
 export default LifeAtCST;
