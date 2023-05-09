87c656cf5e45c7baaca07d8a5a7654bb672fc8aa
popss <pemalw99@gmail.com>
2023-05-09 13:21:24 +0530

    academics hero section

--- frontend/src/pages/Academics.js
+++ frontend/src/pages/Academics.js
@@ -3,43 +3,45 @@ import bgDepartment from "../assets/images/bg-department.png"; // import your he
 
 const Academics = () => {
   return (
-    <div className="hero-section relative">
+  <div>
+      <div className="hero-section relative">
       <img src={bgDepartment} alt="Hero" className="h-854" />
       <div className="absolute top-0 left-0 h-full w-full bg-black opacity-50"></div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 flex divide-x-2">
-        <p className="text-white font-bold mb-2 absolute bottom-0 left-0 p-4">
+      <div className="absolute bottom-0 left-1/4 transform text-center z-10 flex">
+        <p className="text-white font-bold mb-2 p-4 text-right">
           Department
         </p>
-        {/* <div className="h-6 w-px bg-white mb-2"></div> */}
-        <ul className="list-none">
-          <li className="mb-2">
-            <a href="#" className="text-white hover:text-gray-400">
-              Link 1
-            </a>
-          </li>
-          <li className="mb-2">
-            <a href="#" className="text-white hover:text-gray-400">
-              Link 2
-            </a>
-          </li>
-          <li className="mb-2">
-            <a href="#" className="text-white hover:text-gray-400">
-              Link 3
-            </a>
-          </li>
-          <li className="mb-2">
-            <a href="#" className="text-white hover:text-gray-400">
-              Link 4
-            </a>
-          </li>
-          <li className="mb-2">
-            <a href="#" className="text-white hover:text-gray-400">
-              Link 5
-            </a>
-          </li>
-        </ul>
+        <div className="h-72 w-2 bg-white mb-2 self-center"></div>
+        <div className="ml-4 text-left leading-loose">
+          <ul className="list-none">
+            <li className="mb-2">
+              <a href="#" className="text-white hover:text-gray-400">
+                Department of Science and Humanities</a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-white hover:text-gray-400">
+                Department of Civil</a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-white hover:text-gray-400">
+                Department of Electronics and Communication</a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-white hover:text-gray-400">
+                Department of Architecture</a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-white hover:text-gray-400">
+                Department of Information and Technology</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
+    <div>
+      content
+    </div>
+  </div>
   );
 };
 
