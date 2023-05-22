831bab5ad319c6913edccb8690f1f912237adc4f
Divyash Chhetri <divyashchhetri@gmail.com>
2023-05-22 11:14:18 +0600

    Git Push for Pravaat

--- frontend/src/components/Organogram.js
+++ frontend/src/components/Organogram.js
@@ -36,12 +36,12 @@
 //         <div className="p-3 h-24  rounded-sm bg-[#367c9e] flex items-center justify-center">
 //           <span className="text-white">{node.position}</span>
 //         </div>
-//         <div className="text-center mt-2">
+//         <div className="mt-2 text-center">
 //           <span>{node.name}</span>
-//           <span className="text-gray-500 block">{node.position}</span>
+//           <span className="block text-gray-500">{node.position}</span>
 //         </div>
 //         {hasReports && (
-//           <div className="mt-2 flex-grow flex">
+//           <div className="flex flex-grow mt-2">
 //             {organogramData
 //               .filter((n) => n.reportsTo === node.id)
 //               .map((childNode) => renderNode(childNode))}
@@ -51,11 +51,10 @@
 //     );
 //   };
 
-//   return <div className="flex flex-wrap justify-center items-center">{renderNode(organogramData[0])}</div>;
+//   return <div className="flex flex-wrap items-center justify-center">{renderNode(organogramData[0])}</div>;
 // };
 
 // export default Organogram;
-
 
 // import React from 'react';
 // import '../assets/css/organization-chart.css';
@@ -66,16 +65,16 @@
 //       <h1>Responsive Organization Chart</h1>
 //       <figure className="org-chart cf">
 //         <ul className="administration">
-//           <li>					
+//           <li>
 //             <ul className="director">
 //               <li>
 //                 <a href="#"><span>Director</span></a>
 //                 <ul className="subdirector">
 //                   <li><a href="#"><span>Assistante Director</span></a></li>
 //                 </ul>
-//                 <ul className="departments cf">								
+//                 <ul className="departments cf">
 //                   <li><a href="#"><span>Administration</span></a></li>
-                  
+
 //                   <li className="department dep-a">
 //                     <a href="#"><span>Department A</span></a>
 //                     <ul className="sections">
@@ -135,134 +134,319 @@
 
 // export default Organization;
 
+// import React from 'react';
+// //import '../assets/css/organization-chart.css';
 
-import React from 'react';
-//import '../assets/css/organization-chart.css';
+// const organizationData = {
+//   name: 'Director',
+//   subordinates: [
+//     {
+//       name: 'Assistant Director',
+//       subordinates: []
+//     }
+//   ],
+//   departments: [
+//     {
+//       name: 'Administration',
+//       sections: []
+//     },
+//     {
+//       name: 'Department A',
+//       sections: [
+//         { name: 'Section A1' },
+//         { name: 'Section A2' },
+//         { name: 'Section A3' },
+//         { name: 'Section A4' },
+//         { name: 'Section A5' }
+//       ]
+//     },
+//     {
+//       name: 'Department B',
+//       sections: [
+//         { name: 'Section B1' },
+//         { name: 'Section B2' },
+//         { name: 'Section B3' },
+//         { name: 'Section B4' }
+//       ]
+//     },
+//     {
+//       name: 'Department C',
+//       sections: [
+//         { name: 'Section C1' },
+//         { name: 'Section C2' },
+//         { name: 'Section C3' },
+//         { name: 'Section C4' }
+//       ]
+//     },
+//     {
+//       name: 'Department D',
+//       sections: [
+//         { name: 'Section D1' },
+//         { name: 'Section D2' },
+//         { name: 'Section D3' },
+//         { name: 'Section D4' },
+//         { name: 'Section D5' },
+//         { name: 'Section D6' }
+//       ]
+//     },
+//     {
+//       name: 'Department E',
+//       sections: [
+//         { name: 'Section E1' },
+//         { name: 'Section E2' },
+//         { name: 'Section E3' }
+//       ]
+//     }
+//   ]
+// };
 
-const organizationData = {
-  name: 'Director',
-  subordinates: [
-    {
-      name: 'Assistant Director',
-      subordinates: []
-    }
-  ],
-  departments: [
-    {
-      name: 'Administration',
-      sections: []
-    },
-    {
-      name: 'Department A',
-      sections: [
-        { name: 'Section A1' },
-        { name: 'Section A2' },
-        { name: 'Section A3' },
-        { name: 'Section A4' },
-        { name: 'Section A5' }
-      ]
-    },
-    {
-      name: 'Department B',
-      sections: [
-        { name: 'Section B1' },
-        { name: 'Section B2' },
-        { name: 'Section B3' },
-        { name: 'Section B4' }
-      ]
-    },
-    {
-      name: 'Department C',
-      sections: [
-        { name: 'Section C1' },
-        { name: 'Section C2' },
-        { name: 'Section C3' },
-        { name: 'Section C4' }
-      ]
-    },
-    {
-      name: 'Department D',
-      sections: [
-        { name: 'Section D1' },
-        { name: 'Section D2' },
-        { name: 'Section D3' },
-        { name: 'Section D4' },
-        { name: 'Section D5' },
-        { name: 'Section D6' }
-      ]
-    },
-    {
-      name: 'Department E',
-      sections: [
-        { name: 'Section E1' },
-        { name: 'Section E2' },
-        { name: 'Section E3' }
-      ]
-    }
-  ]
+// const Organization = () => {
+//   const renderSubordinates = (subordinates) => {
+//     if (subordinates.length === 0) return null;
+
+//     return (
+//       <ul className="subdirector">
+//         {subordinates.map((subordinate, index) => (
+//           <li key={index}>
+//             <a href="#"><span>{subordinate.name}</span></a>
+//             {renderSubordinates(subordinate.subordinates)}
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   };
+
+//   const renderSections = (sections) => {
+//     if (sections.length === 0) return null;
+
+//     return (
+//       <ul className="sections">
+//         {sections.map((section, index) => (
+//           <li className="section" key={index}><a href="#"><span>{section.name}</span></a></li>
+//         ))}
+//       </ul>
+//     );
+//   };
+
+//   const renderDepartments = (departments) => {
+//     if (departments.length === 0) return null;
+
+//     return (
+//       <ul className="departments cf">
+//         {departments.map((department, index) => (
+//           <li className={`department dep-${index}`} key={index}>
+//             <a href="#"><span>{department.name}</span></a>
+//             {renderSections(department.sections)}
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   };
+
+//   return (
+//     <div className="content">
+//       <h1>Responsive Organization Chart</h1>
+//       <figure className="org-chart cf">
+//         <ul className="administration">
+//           <li>
+//             <ul className="director">
+//               <li>
+//                 <a href="#"><span>{organizationData.name}</span></a>
+//                 {renderSubordinates(organizationData.subordinates)}
+//                 {renderDepartments(organizationData.departments)}
+//               </li>
+//             </ul>
+//           </li>
+//         </ul>
+//       </figure>
+//     </div>
+//   );
+// };
+
+// export default Organization;
+
+import React from "react";
+
+const Organogram = () => {
+	return (
+		<div className="content">
+			<h1 className="text-center">Responsive Organization Chart</h1>
+			<figure className="org-chart cf">
+				<ul className="administration">
+					<li>
+						<ul className="director">
+							<li>
+								<a href="#">
+									<span>Director</span>
+								</a>
+								<ul className="subdirector">
+									<li>
+										<a href="#">
+											<span>Assistant Director</span>
+										</a>
+									</li>
+								</ul>
+								<ul className="departments cf">
+									<li>
+										<a href="#">
+											<span>Administration</span>
+										</a>
+									</li>
+
+									{/* Add your departments here */}
+									<li className="department dep-a">
+										<a href="#">
+											<span>Department A</span>
+										</a>
+										<ul className="sections">
+											<li className="section">
+												<a href="#">
+													<span>Section A1</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section A2</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section A3</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section A4</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section A5</span>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="department dep-b">
+										<a href="#">
+											<span>Department B</span>
+										</a>
+										<ul className="sections">
+											<li className="section">
+												<a href="#">
+													<span>Section B1</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section B2</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section B3</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section B4</span>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="department dep-c">
+										<a href="#">
+											<span>Department C</span>
+										</a>
+										<ul className="sections">
+											<li className="section">
+												<a href="#">
+													<span>Section C1</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section C2</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section C3</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section C4</span>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="department dep-d">
+										<a href="#">
+											<span>Department D</span>
+										</a>
+										<ul className="sections">
+											<li className="section">
+												<a href="#">
+													<span>Section D1</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section D2</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section D3</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section D4</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section D5</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section D6</span>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="department dep-e">
+										<a href="#">
+											<span>Department E</span>
+										</a>
+										<ul className="sections">
+											<li className="section">
+												<a href="#">
+													<span>Section E1</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section E2</span>
+												</a>
+											</li>
+											<li className="section">
+												<a href="#">
+													<span>Section E3</span>
+												</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</figure>
+		</div>
+	);
 };
 
-const Organization = () => {
-  const renderSubordinates = (subordinates) => {
-    if (subordinates.length === 0) return null;
-
-    return (
-      <ul className="subdirector">
-        {subordinates.map((subordinate, index) => (
-          <li key={index}>
-            <a href="#"><span>{subordinate.name}</span></a>
-            {renderSubordinates(subordinate.subordinates)}
-          </li>
-        ))}
-      </ul>
-    );
-  };
-
-  const renderSections = (sections) => {
-    if (sections.length === 0) return null;
-
-    return (
-      <ul className="sections">
-        {sections.map((section, index) => (
-          <li className="section" key={index}><a href="#"><span>{section.name}</span></a></li>
-        ))}
-      </ul>
-    );
-  };
-
-  const renderDepartments = (departments) => {
-    if (departments.length === 0) return null;
-
-    return (
-      <ul className="departments cf">
-        {departments.map((department, index) => (
-          <li className={`department dep-${index}`} key={index}>
-            <a href="#"><span>{department.name}</span></a>
-            {renderSections(department.sections)}
-          </li>
-        ))}
-      </ul>
-    );
-  };
-
-  return (
-    <div className="content">
-      <h1>Responsive Organization Chart</h1>
-      <figure className="org-chart cf">
-        <ul className="administration">
-          <li>
-            <ul className="director">
-              <li>
-                <a href="#"><span>{organizationData.name}</span></a>
-                {renderSubordinates(organizationData.subordinates)}
-                {renderDepartments(organizationData.departments)}
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </figure>
-    </div>
-  );
-};
-
-export default Organization;
+export default Organogram;
--- frontend/src/pages/Programmes.js
+++ frontend/src/pages/Programmes.js
@@ -2,7 +2,11 @@ import React from "react";
 import Organogram from "../components/Organogram";
 
 const Programmes = () => {
-	return <div><Organogram/></div>;
+	return (
+		<div>
+			<Organogram />
+		</div>
+	);
 };
 
 export default Programmes;
