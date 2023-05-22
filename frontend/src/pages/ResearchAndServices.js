80feeaa9f61063f91c33e39c06ec1c36eecd5784
thinley-choden <120625075+thinley-choden@users.noreply.github.com>
2023-05-22 21:13:36 +0600

    research and services
    edited-research and services

--- frontend/src/pages/ResearchAndServices.js
+++ frontend/src/pages/ResearchAndServices.js
@@ -66,7 +66,7 @@ const ResearchAndServices = () => {
   <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-3">
     <div className="h-80 bg-[#eee] rounded-lg shadow-lg p-4">
     <h1 className="font-bold">Center for renewable and Sustainable Energy Development(CRSED)</h1>
-    <button className="bg-[#ffcocb] p-3 font-semibold mt-[120px] self-end mt-12"><a href="#_" class="relative inline-flex items-center justify-center p-4 px-4 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
+    <button className="bg-[#ffcocb] p-3 font-semibold mt-[120px] self-end mt-12 ml-6"><a href="#_" class="relative inline-flex items-center justify-center p-4 px-4 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-500 group-hover:translate-x-0 ease">
 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
 </span>
@@ -78,7 +78,7 @@ const ResearchAndServices = () => {
   <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-3">
     <div className="h-65 bg-[#eee] rounded-lg shadow-lg p-4">
     <h1 className="font-bold">Center for Disaster Risk Reduction and Community Development Studies(CDRRCDS)</h1>
-    <button className="bg-[#ffcocb] p-3 font-semibold mt-[120px] self-end mt-12"><a href="#_" class="relative inline-flex items-center justify-center p-4 px-4 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
+    <button className="bg-[#ffcocb] p-3 font-semibold mt-[120px] self-end mt-12 ml-6"><a href="#_" class="relative inline-flex items-center justify-center p-4 px-4 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-500 group-hover:translate-x-0 ease">
 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
 </span>
@@ -90,7 +90,7 @@ const ResearchAndServices = () => {
   <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-3">
     <div className="h-80 bg-[#eee] rounded-lg shadow-lg p-4">
     <h1 className="font-bold">Center for renewable and Sustainable Energy Development(CRSED)</h1>
-    <button className="bg-[#ffcocb] p-3 font-semibold mt-[120px] self-end mt-12"><a href="#_" class="relative inline-flex items-center justify-center p-4 px-4 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
+    <button className="bg-[#ffcocb] p-3 font-semibold mt-[120px] self-end mt-12 ml-6"><a href="#_" class="relative inline-flex items-center justify-center p-4 px-4 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-500 group-hover:translate-x-0 ease">
 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
 </span>
