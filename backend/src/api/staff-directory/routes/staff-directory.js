5ffc37acce8631498dd5452bd3f1aaae839c7842
popss <pemalw99@gmail.com>
2023-05-23 16:39:40 +0530

    Created Schema

---
+++ backend/src/api/staff-directory/routes/staff-directory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * staff-directory router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::staff-directory.staff-directory');
